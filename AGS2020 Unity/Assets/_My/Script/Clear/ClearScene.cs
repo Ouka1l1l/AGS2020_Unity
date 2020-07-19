3ae1bef74a3013b394ead7b4f96447e0100859aa
miyagawa <tekito@kk.kk>
2020-07-19 15:20:41 +0900

    クリアシーン

---
+++ AGS2020 Unity/Assets/_My/Script/Clear/ClearScene.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class ClearScene : MonoBehaviour
+{
+    private PlayableDirector _playableDirector;
+
+    private bool _timelineEnd = false;
+
+    [SerializeField]
+    private Canvas _blinkCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _timelineEnd = false;
+
+        _playableDirector = GetComponent<PlayableDirector>();
+
+        _playableDirector.stopped += OnPlayableDirectorStopped;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!_timelineEnd)
+        {
+            return;
+        }
+
+        if(Input.GetButtonDown("Submit"))
+        {
+            SceneManager.LoadScene("Hub");
+        }
+    }
+
+    private void OnPlayableDirectorStopped(PlayableDirector aDirector)
+    {
+        _timelineEnd = true;
+        _blinkCanvas.gameObject.SetActive(true);
+    }
+}
