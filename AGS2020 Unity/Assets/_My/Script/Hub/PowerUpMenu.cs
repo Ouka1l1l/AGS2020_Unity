560bd0c201d9a351b587f65f7d1e4784464d1b8f
miya <aoitak221287@icloud.com>
2020-07-27 13:56:13 +0900

    イベントトリガーのイベントを切り替えよう

---
+++ AGS2020 Unity/Assets/_My/Script/Hub/PowerUpMenu.cs
@@ -0,0 +1,43 @@
+﻿using Packages.Rider.Editor.UnitTesting;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PowerUpMenu : MonoBehaviour
+{
+    [SerializeField]
+    private EventTrigger _eventTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Test()
+    {
+        foreach(var trigger in _eventTrigger.triggers)
+        {
+            if(trigger.eventID != EventTriggerType.Select)
+            {
+                continue;
+            }
+
+            EventTrigger.Entry entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.Select;
+
+            ////////////////呼ぶ関数を切り替えて
+        }
+    }
+
+    private void OnSelect()
+    {
+        Debug.Log("決定");
+    }
+}
